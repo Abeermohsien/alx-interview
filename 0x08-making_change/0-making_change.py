1294e1dd13d14d192d285d35fa298bec791ccbe0
Abeermohsien <aboorakhalil993@gmail.com>
2024-08-22 09:37:14 -0700

    $

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""make chage module"""
+
+def makeChange(coins, total):
+    """
+    fewest num in coin
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
